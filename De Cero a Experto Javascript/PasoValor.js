a30bdc6ce01278c9542df9f68bd5b7bda69db200
Uterdev <danielconejo1985@gmail.com>
2024-06-18 01:28:00 +0200

    Paso por valor

---
+++ De Cero a Experto Javascript/PasoValor.js
@@ -0,0 +1,12 @@
+// Paso Valor en JS
+// Pasando informacion de tipo primitivo (number, bool, string)
+
+function cambiarValor(parametro) {
+    console.log(parametro);
+    parametro = 20;
+    console.log(parametro);
+}
+
+let argumento = 10;
+cambiarValor(argumento);
+console.log(argumento);
