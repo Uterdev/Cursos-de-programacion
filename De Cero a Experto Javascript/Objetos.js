1716dd2bc0857539b9eb7607b5d6cb90ce4ed51a
Uterdev <danielconejo1985@gmail.com>
2024-06-19 03:03:37 +0200

    Objetos

---
+++ De Cero a Experto Javascript/Objetos.js
@@ -0,0 +1,18 @@
+let x = 10;
+console.log(x.length); 
+// un valor primitivo no se puede considerar como objeto
+
+let persona = {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    email: 'jperez@mail.com',
+    edad: 28,
+}
+
+console.log(persona.nombre);
+console.log(persona.apellido);
+console.log(persona.email);
+console.log(persona.edad);
+
+console.log(persona);
+
