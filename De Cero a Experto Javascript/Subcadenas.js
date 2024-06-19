6e34ba9b1ff22d2ed4d13dfdc9ada56eba195433
Uterdev <danielconejo1985@gmail.com>
2024-06-19 02:03:11 +0200

    Subcadenas

---
+++ De Cero a Experto Javascript/Subcadenas.js
@@ -0,0 +1,11 @@
+// Subcadenas en JS
+let cadena3 = 'Hola Mundo';
+
+// substring(indice_inicio, indice_fin)
+// No se incluye el caracter indice_fin
+let cadena1 = cadena3.substring(0, 4);
+console.log(cadena1);
+
+// Ejercicio para extraer la subcadena Mundo de la variable cadena3
+let cadena2 = cadena3.substring(5, 10);
+console.log(cadena2);
