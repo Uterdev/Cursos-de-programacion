2bb1a3b8ef7c488ac0bd14d8f56f06a37e829a86
Uterdev <danielconejo1985@gmail.com>
2024-06-19 02:25:37 +0200

    Convertir Número a Cadena

---
+++ De Cero a Experto Javascript/Convertir Numero a Cadena.js
@@ -0,0 +1,7 @@
+// Convertir Número a Cadena en JS
+let a = 10, b = 20;
+// Si son valores numericos se suman
+console.log(a + b);
+// Convertir número a cadena
+let concatenacion = a.toString() + b.toString();
+console.log(concatenacion);
