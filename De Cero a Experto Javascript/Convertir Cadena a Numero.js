bb16a473d5040a59c91a190164350ec0a1ac62aa
Uterdev <danielconejo1985@gmail.com>
2024-06-19 02:20:34 +0200

    Convertir Cadena a Numero

---
+++ De Cero a Experto Javascript/Convertir Cadena a Numero.js
@@ -0,0 +1,9 @@
+// Convertir de cadena a numero en JS
+let a ='10', b ='20';
+// Si son cadenas se concatenan
+console.log(a + b);
+
+// Convertir a numero para que se sumen
+let suma = parseInt(a) + parseInt(b);
+console.log(suma);
+// la funcion integrada en JS "parseInt" convierte un string a numero
