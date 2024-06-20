17b8e432ff2d555001287cb89e93888d611d7af9
Uterdev <danielconejo1985@gmail.com>
2024-06-21 01:08:29 +0200

    organizando

---
+++ De Cero a Experto Javascript/Variables/Variable.js
@@ -0,0 +1,69 @@
+// Ejemplos de variables en Javascript.
+
+var miNumero1 = 15;
+console.log(miNumero1);
+
+let miNumero2 = 20;
+console.log(miNumero2);
+
+let miVariable1 = ("Daniel");
+
+
+// Regalas de nombres de variables
+
+//letras, $, _, posteriormente se pueden usar nomeros
+
+let $miNumero1 = 10;
+console.log($miNumero1);
+
+let _miNumero2 = 20;
+console.log(_miNumero2);
+
+let miNumero3 = 30;
+console.log(miNumero3);
+
+// Javascript es sensible a mayusculas y minusculas
+
+// console.log(MiNumero3);
+
+/*
+Comentarios
+de varias
+lineas
+*/
+
+// Tipos de datos en JS
+
+let miEntero = 10;
+console.log(miEntero);
+console.log(typeof miEntero);
+
+miEntero = "Hola";
+console.log(typeof miEntero);
+
+
+let miFlotante = 7.9;
+console.log(miFlotante);
+console.log(typeof miFlotante);
+
+
+let miCadena = "Hola";
+console.log(miCadena);
+console.log(typeof miCadena);
+
+
+let miBoolean = true; //true o false
+console.log(miBoolean);
+console.log(typeof miBoolean);
+
+
+let miNull = null;
+console.log(miNull);
+console.log(typeof miNull);
+
+
+let miUndefined = undefined
+console.log(miUndefined);
+console.log(typeof miUndefined);
+
+
