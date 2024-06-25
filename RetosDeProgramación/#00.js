3be3c6ad3b19805c500488e3c0e9f4bb0a1514b5
Uterdev <danielconejo1985@gmail.com>
2024-06-25 02:20:34 +0200

    #00

---
+++ RetosDeProgramación/#00.js
@@ -0,0 +1,45 @@
+/*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+
+// vamos a hacer los retos de la pagina www.retosdeprogramacion.com
+// vamos a usar el lenguaje www.javascript.com 
+
+/*
+esto es otra forma
+de hacer un comentario
+*/
+
+var variable = '5';
+let constante = "Daniel";
+
+let miCadena = "Juan";
+let miEntero = 1;
+let miFlotante = 1.6;
+let miBoolean = true;
+let miNull = null;
+let miUndefined = undefined;
+
+console.log('hola, javascript');
+
+
