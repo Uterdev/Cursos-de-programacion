8893fac59d4769cc35c5752a3c03fe68c1ac9ca8
Uterdev <danielconejo1985@gmail.com>
2024-06-27 02:44:14 +0200

    Clases

--- De Cero a Experto Javascript/Clases/Clases.js
+++ De Cero a Experto Javascript/Clases/Clases.js
@@ -9,6 +9,7 @@ class Persona{
 let persona1 = new Persona("Juan", "Alvarez");
 console.log(persona1);
 
-
+persona1.nombre = "Juan Carlos" ;
 let persona2 = new Persona("Carlos", "Lara");
 console.log(persona2);
+console.log(persona1);
